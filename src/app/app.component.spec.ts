cb2148e2d5833cd46cfcf2e8bcfaed6a88a42142
clampr <christian.lamprecht@aol.de>
2021-04-25 22:54:24 +0200

    Basi Navbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'meteostat-angular'`, () => {
+  it(`should have as title 'Meteostat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meteostat-angular');
+    expect(app.title).toEqual('Meteostat');
   });
 
   it('should render title', () => {
